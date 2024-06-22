beebd51c0f1a836476e4be922bcdad3ae78ceec8
W100552733w <160983509+W100552733w@users.noreply.github.com>
2024-06-22 22:15:21 +0800

    Create Odd.js

---
+++ hw3/Odd.js
@@ -0,0 +1,14 @@
+function printOdd(n){
+    for(let i=1;i<=n;i++){
+        if(i%2==1){
+            console.log(i," ");
+        }
+    }
+}
+printOdd(10)
+printOdd(1000)
+
+printOdd(100)
+printOdd(160)
+
+
